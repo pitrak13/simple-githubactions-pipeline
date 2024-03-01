b47f1a5efd1fa36b4fe267f127be15a411edcb3e
pitrak13 <mospatrik@gmail.com>
2024-03-01 16:01:41 +0000

    changing background color

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>bigus di</h1>')
+  res.send('<h1 style="background-color:Gray;">bigus di</h1>')
 })
 
 const PORT = 8080
